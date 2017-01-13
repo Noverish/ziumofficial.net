b5a7b6bd5c6c236decb956133283908057f4e45c
Noverish <embrapers263@gmail.com>
2017-01-13 23:55:07 +0900

    Finish event_info.php

---
+++ api/event_info.php
@@ -0,0 +1,14 @@
+<?php
+    include('config.php');
+
+    $event_id = $_POST["event_id"];
+
+    $sql = "SELECT title, content, img_main, event_start, event_end, date FROM Event WHERE _id = $event_id";
+    $result = mysqli_query($conn, $sql) or print_error_and_die(mysqli_error($conn));
+    $tmp = query_result_to_array($result);
+    $res = $tmp[0];
+
+    $res = array_merge(array('res' => 1, 'msg' => 'success'), $res);
+
+    echo raw_json_encode($res);
+?>
