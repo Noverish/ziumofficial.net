dd35ed5a4bcf1fc4597f9215ac9d373aadc93b08
Noverish <embrapers263@gmail.com>
2017-01-13 23:54:48 +0900

    store_report 에서 문자열 ''로 안 감싼 문제 해결

--- api/store_report.php
+++ api/store_report.php
@@ -6,7 +6,7 @@
     $content = $_POST["content"];
 
     $now_str = date('Y-m-d H:i:s');
-    $sql = "INSERT INTO StoreReport (user_id, store_id, content, date) VALUES ($user_id, $store_id, $content, '$now_str')";
+    $sql = "INSERT INTO StoreReport (user_id, store_id, content, date) VALUES ($user_id, $store_id, '$content', '$now_str')";
     $result = mysqli_query($conn, $sql) or print_error_and_die(mysqli_error($conn));
 
     $res["res"] = 1;
