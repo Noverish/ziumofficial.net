746da02554472d45e30845b8d2e48e29bc511926
Noverish <embrapers263@gmail.com>
2017-01-15 11:09:44 +0900

    Split pages in event_list, and rcmd_list

--- api/event_list.php
+++ api/event_list.php
@@ -14,7 +14,7 @@
 
     $res["res"] = 1;
     $res["msg"] = "success";
-    $res["data"] = $data;
+    $res["data"] = array_slice($data,$PAGE_SIZE * ($page - 1),$PAGE_SIZE);
 
     echo raw_json_encode($res);
 ?>
