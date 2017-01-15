2252c22c37e92bb628792a7958391febd34a5b7c
Noverish <embrapers263@gmail.com>
2017-01-15 11:46:38 +0900

    order by write date desc in store_review

--- api/store_review.php
+++ api/store_review.php
@@ -6,7 +6,7 @@
     $page = $_POST["page"];
     $page_offset = $PAGE_SIZE * ($page - 1);
 
-    $sql_review = "SELECT * FROM Review WHERE store_id = $store_id LIMIT $page_offset, $PAGE_SIZE";
+    $sql_review = "SELECT * FROM Review WHERE store_id = $store_id ORDER BY write_date DESC LIMIT $page_offset, $PAGE_SIZE";
     $result_review = mysqli_query($conn, $sql_review) or print_error_and_die(mysqli_error($conn));
 
     $reviews = get_review_array($result_review);
