d3b6cf0419684b7b91484f69a4f036002a1ab751
Noverish <embrapers263@gmail.com>
2017-01-25 18:34:26 +0900

    Finish user_rank.php

---
+++ api/user_rank.php
@@ -0,0 +1,14 @@
+<?php
+    include("config.php");
+
+    $DISPLAY_USER_NUM = 10;
+
+    $sql = "SELECT _id, user_name, is_owner, score FROM User ORDER BY score DESC LIMIT $DISPLAY_USER_NUM";
+    $result = mysqli_query($conn, $sql) or print_error_and_die(mysqli_error($conn));
+
+    $response["res"] = 1;
+    $response["msg"] = "success";
+    $response["user_id"] = query_result_to_array($result);
+
+    echo raw_json_encode($response);
+?>
