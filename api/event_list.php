c38ed33d8a4a85e5f4c2656c2b3a7527f238fdf4
Noverish <embrapers263@gmail.com>
2017-01-30 18:43:36 +0900

    Adjust event_list.php fit to Server

--- api/event_list.php
+++ api/event_list.php
@@ -4,14 +4,18 @@
     $page = $_POST["page"];
     $page_offset = $PAGE_SIZE * ($page - 1);
 
-    $sql="SELECT _id, img_title FROM Event ORDER BY priority DESC, date DESC LIMIT $page_offset, $PAGE_SIZE";
+    $sql="SELECT Event._id AS event_id, Event.store_id, Store.name AS store_name, Event.content, Event.is_open, Event.date ".
+         "FROM Event ".
+         "INNER JOIN Store ON Event.store_id = Store._id ".
+         "ORDER BY Event.priority DESC, Event.date DESC ".
+         "LIMIT $page_offset, $PAGE_SIZE";
     $result = mysqli_query($conn, $sql) or print_error_and_die(mysqli_error($conn));
     $data = query_result_to_array($result);
 
-    array_walk($data, function (& $item) {
-        $item = array('event_id' => $item['_id']) + $item;
-        unset($item['_id']);
-    });
+    // array_walk($data, function (& $item) {
+    //     $item = array('event_id' => $item['_id']) + $item;
+    //     unset($item['_id']);
+    // });
 
     $res["res"] = 1;
     $res["msg"] = "success";
