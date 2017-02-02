542030dd18fca4780b55e998a604bed7c9b5385f
Noverish <embrapers263@gmail.com>
2017-02-02 16:13:58 +0900

    store_review에 sort 추가

--- api/store_review.php
+++ api/store_review.php
@@ -3,14 +3,16 @@
 
     ($store_id = $_POST["store_id"]) != NULL or print_error_and_die("There is no store_id");
     ($page = $_POST["page"]) != NULL or print_error_and_die("There is no page");
+    ($sort = $_POST["sort"]) != NULL or print_error_and_die("There is no sort");
 
     if(!is_numeric($store_id)) print_error_and_die("store_id is not number");
     if(!is_numeric($page)) print_error_and_die("page is not number");
+    if(!is_numeric($sort)) print_error_and_die("sort is not number");
 
     $page_offset = $PAGE_SIZE * ($page - 1);
 
     $sql_review =
-        "SELECT Store._id as store_id, Store.name as store_name, ".
+        "SELECT Review._id as review_id, Store._id as store_id, Store.name as store_name, ".
         "(SELECT IFNULL(AVG(star_rate), 0) FROM Review WHERE store_id = Store._id) as star_average, ".
         "(SELECT COUNT(_id) FROM Review WHERE store_id = Store._id) as review_num, ".
         "(SELECT COUNT(_id) FROM UserDibs WHERE store_id = Store._id) as dibs_num, ".
@@ -20,9 +22,16 @@
         "FROM Review ".
         "INNER JOIN Store ON Store._id = Review.store_id ".
         "INNER JOIN User ON User._id = Review.user_id ".
-        "WHERE store_id = $store_id ".
-        "ORDER BY write_date DESC ".
-        "LIMIT $page_offset, $PAGE_SIZE";
+        "WHERE store_id = $store_id ";
+
+    if($sort == 0)
+        $sql_review .= "ORDER BY like_num DESC ";
+    else if($sort == 1)
+        $sql_review .= "ORDER BY write_date DESC ";
+    else
+        print_error_and_die("sort MUST be one of 0, 1");
+
+    $sql_review .= "LIMIT $page_offset, $PAGE_SIZE ";
     $result_review = mysqli_query($conn, $sql_review) or print_sql_error_and_die($conn, $sql_review);
 
     if($result_review == null || mysqli_num_rows($result_review) == 0)
