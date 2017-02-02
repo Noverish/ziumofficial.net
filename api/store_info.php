a021c0adcd469f9608a3d0074e9f412e615b2607
Noverish <embrapers263@gmail.com>
2017-02-02 12:04:29 +0900

    resolve typo of store_info.php

--- api/store_info.php
+++ api/store_info.php
@@ -5,7 +5,7 @@
 
     if(!is_numeric($store_id)) print_error_and_die("store_id is not number");
 
-    $sql_store = "SELECT *, Event.img_main, Event.img_detail, ".
+    $sql_store = "SELECT *, Event.img_main as event_main, Event.img_detail as event_detail, ".
                  "(SELECT AVG(star_rate) FROM Review WHERE store_id = $store_id) as star_average, ".
                  "(SELECT COUNT(_id) FROM Review WHERE store_id = $store_id) as review_num,".
                  "(SELECT COUNT(_id) FROM UserDibs WHERE store_id = $store_id) as dibs_num ".
@@ -15,7 +15,7 @@
     $result_store = mysqli_query($conn, $sql_store) or print_sql_error_and_die($conn, $sql_store);
     $res = mysqli_fetch_assoc($result_store);
 
-    if($result == null || mysqli_num_rows($res) == 0)
+    if($result_store == null || mysqli_num_rows($result_store) == 0)
         print_error_and_die("There is no store whose id is ".$store_id);
 
     $sql_img = "SELECT * FROM StoreImage WHERE store_id = $store_id ORDER BY priority DESC";
