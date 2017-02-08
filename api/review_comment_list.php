5f653c8bae994f864146056ee62563d0111436fa
Noverish <embrapers263@gmail.com>
2017-02-09 02:03:48 +0900

    add page in review_comment_list

--- api/review_comment_list.php
+++ api/review_comment_list.php
@@ -3,8 +3,13 @@
     include('query_func.php');
 
     ($review_id = $_POST["review_id"]) != NULL or print_error_and_die("There is no review_id");
+    ($page = $_POST["page"]) != NULL or print_error_and_die("There is no page");
 
     if(!is_numeric($review_id)) print_error_and_die("review_id is not number");
+    if(!is_numeric($page)) print_error_and_die("page is not number");
+
+    if($page < 1) print_error_and_die("page must be bigger than 0");
+    $page_offset = $PAGE_SIZE * ($page - 1);
 
     if_not_valid_review_id_then_die($review_id);
 
@@ -12,7 +17,8 @@
         "SELECT Comment._id as comment_id, user_id, User.user_name, User.is_owner, content, date ".
         "FROM Comment ".
         "INNER JOIN User ON User._id = Comment.user_id ".
-        "WHERE review_id = $review_id";
+        "WHERE review_id = $review_id ".
+        "LIMIT $page_offset, $PAGE_SIZE ";
     $result = mysqli_query($conn, $sql) or print_error_and_die(mysqli_error($conn));
 
     $res["res"] = 1;
