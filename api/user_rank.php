964e45230fe03848b0f20da84acaaba4523c5fc9
Noverish <embrapers263@gmail.com>
2017-04-05 17:24:34 +0900

    rank list num from 10 to 50

--- api/user_rank.php
+++ api/user_rank.php
@@ -1,7 +1,7 @@
 <?php
     include("config.php");
 
-    $DISPLAY_USER_NUM = 10;
+    $DISPLAY_USER_NUM = 50;
 
     $sql = "SELECT _id as user_id, user_name, is_owner, score FROM User ORDER BY score DESC LIMIT $DISPLAY_USER_NUM";
     $result = mysqli_query($conn, $sql) or print_error_and_die(mysqli_error($conn));
