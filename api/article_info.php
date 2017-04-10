acf7321ef4f05c4482d977218784f8a4909797b4
Noverish <embrapers263@gmail.com>
2017-04-11 01:42:28 +0900

    article_info insert history to HistoryArticle table

--- api/article_info.php
+++ api/article_info.php
@@ -1,10 +1,11 @@
 <?php
     include('config.php');
 
-    ($user_id = $_POST["user_id"]) != NULL or print_error_and_die("There is no user_id");
     ($article_id = $_POST["article_id"]) != NULL or print_error_and_die("There is no article_id");
+    ($user_id = $_POST["user_id"]) != NULL or $user_id = -1;
+    ($is_android = $_POST["is_android"]) != NULL or $is_android = -1;
+    ($app_version = $_POST["app_version"]) != NULL or $app_version = "0.0.0";
 
-    if(!is_numeric($user_id)) print_error_and_die("user_id is not number");
     if(!is_numeric($article_id)) print_error_and_die("article_id is not number");
 
     $sql="SELECT *, (SELECT COUNT(*) FROM ArticleLike WHERE user_id = $user_id && article_id = ViewArticle.article_id) AS is_user_liked FROM ViewArticle WHERE article_id = $article_id";
@@ -15,4 +16,7 @@
     $row["msg"] = "success";
 
     echo raw_json_encode($row);
+
+    $sql = "INSERT INTO HistoryArticle (article_id, user_id, date, is_android, app_version) VALUES ($article_id, $user_id, now(), $is_android, '$app_version')";
+    $result = mysqli_query($conn, $sql);
 ?>
