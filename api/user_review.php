1821cdd450cbdf012713b3c7e9fbc0bb71c0b38c
Noverish <embrapers263@gmail.com>
2017-05-07 22:00:10 +0900

    user_like 와 user_review에서 보고자 하는 사람의 user_id 외에 사용자의 user_id를 user_token으로 받아옴

--- api/user_review.php
+++ api/user_review.php
@@ -4,6 +4,7 @@
 
     ($user_id = $_POST["user_id"]) != NULL or print_error_and_die("There is no user_id");
     ($page = $_POST["page"]) != NULL or print_error_and_die("There is no page");
+    ($user_token = $_POST["user_token"]) != NULL or $user_token = $user_id;
 
     if(!is_numeric($user_id)) print_error_and_die("user_id is not number");
     if(!is_numeric($page)) print_error_and_die("page is not number");
@@ -18,7 +19,7 @@
         "user_id, User.user_name, User.is_owner, star_rate, content, img1, img2, img3, ".
         "(SELECT COUNT(_id) FROM UserLikes WHERE review_id = Review._id) as like_num, ".
         "(SELECT COUNT(_id) FROM Comment WHERE review_id = Review._id) as comment_num, write_date, modify_date, ".
-        "(SELECT COUNT(_id) FROM UserLikes WHERE UserLikes.user_id = $user_id && UserLikes.review_id = Review._id) as is_user_liked ".
+        "(SELECT COUNT(_id) FROM UserLikes WHERE UserLikes.user_id = $user_token && UserLikes.review_id = Review._id) as is_user_liked ".
         "FROM Review ".
         "INNER JOIN User ON User._id = Review.user_id ".
         "INNER JOIN Store On Store._id = Review.store_id ".
